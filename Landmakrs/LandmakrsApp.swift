49ddcc9654994c5164bb9445b50a679b0f9d309c
Alexander <a.pride.xxi@tutanota.com>
2023-03-25 10:14:28 +0300

    Initial Commit

---
+++ Landmakrs/LandmakrsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmakrsApp.swift
+//  Landmakrs
+//
+//  Created by Александр Прайд on 25.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmakrsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

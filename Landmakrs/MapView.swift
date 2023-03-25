185ba004fde3dd47603fedc04e297e9c4305f9b3
Alexander <a.pride.xxi@tutanota.com>
2023-03-25 13:39:08 +0300

    Create MapView

---
+++ Landmakrs/MapView.swift
@@ -0,0 +1,32 @@
+//
+//  MapView.swift
+//  Landmakrs
+//
+//  Created by Александр Прайд on 25.03.2023.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+            latitude: 57.43976,
+            longitude: 57.01026
+        ),span: MKCoordinateSpan(
+            latitudeDelta: 0.2,
+            longitudeDelta: 0.2
+        )
+    )
+    
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
